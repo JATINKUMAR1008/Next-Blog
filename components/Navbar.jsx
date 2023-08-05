dc75e3b38ca1510a2c37890d0e4ae11a84e78b66
JATINKUMAR1008 <jatinkumar10082003@gmail.com>
2023-08-05 16:09:11 +0530

    new commit

--- components/Navbar.jsx
+++ components/Navbar.jsx
@@ -39,9 +39,9 @@ export default function Navbar() {
           <li onClick={()=> router.push('/content')}>Home</li>
           <li onClick={()=> router.push('/trending')}>Trending</li>
           <li onClick={()=> router.push('/create')}>Create</li>
-          <li onClick={()=> router.push('/login')}>Login</li>
+          <li onClick={()=> router.push('/auth/login')}>Login</li>
         </ul>
-        <button className={buttons.btn_primary} onClick={()=> router.push('/signup')}>Create an account </button>
+        <button className={buttons.btn_primary} onClick={()=> router.push('/register')}>Create an account </button>
       </nav>
       </header>
     </>
--- models/Blogs_Schema.js
+++ models/Blogs_Schema.js
@@ -4,5 +4,12 @@ const BlogSchema = new mongoose.Schema({
     title: String,
     desc: String,
     author: String,
+    date: String,
+    readTime: String,
+    tags: [
+        {tag: String}
+    ],
+    content: String
+},{timestamps: true})
 
-})
+export default mongoose.model("Blog",BlogSchema)
